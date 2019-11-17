91386799e003c79cb47b3444c1e2c42a4ffa2843
Lukas Hatzenbühler <lukashatzenbuehler@Lukass-MBP.fritz.box>
2019-11-17 14:23:54 +0100

    bj

--- src/indexraffa.js
+++ src/indexraffa.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 /**
  * Klasse PageOverview: Stellt die Startseite der App zur Verfügung
  */
@@ -34,41 +31,6 @@ class Home {
         pageDom.innerHTML = html;
 
 
-=======
-/**
- * Klasse PageOverview: Stellt die Startseite der App zur Verfügung
- */
-class Home {
-    /**
-     * Konstruktor
-     * @param {App} app Zentrale Instanz der App-Klasse
-     */
-    constructor(app) {
-        this._app = app;
-    }
-
-    /**
-     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
-     */
-    async show() {
-        // Anzuzeigenden Seiteninhalt nachladen
-        let html = await fetch("indexraffa.html");
-        let css = await fetch("indexraffa.css");
-
-        if (html.ok && css.ok) {
-            html = await html.text();
-            css = await css.text();
-        } else {
-            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
-            return;
-        }
-
-        // Seite zur Anzeige bringen
-        let pageDom = document.createElement("div");
-        pageDom.innerHTML = html;
-
-
->>>>>>> 9760b80976220f5f2571f28e736e61de844f5fd7
 
         this._app.setPageCss(css);
         this._app.setPageContent(pageDom.querySelector("main"));
