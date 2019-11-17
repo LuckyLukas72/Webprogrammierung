f8d4f3959348ed27d1db89907606ae50c6d52226
Lukas Hatzenbühler <lukashatzenbuehler@Lukass-MBP.fritz.box>
2019-11-17 15:00:22 +0100

    Regeln des Casinos implementiert

---
+++ src/ReglementCasino/rules.js
@@ -0,0 +1,38 @@
+
+class Regeln {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+    }
+
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show() {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("ReglementCasino/rules.html");
+        let css = await fetch("ReglementCasino/rules.css");
+
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+
+
+        this._app.setPageCss(css);
+        this._app.setPageContent(pageDom.querySelector("main"));
+    }
+
+
+}
