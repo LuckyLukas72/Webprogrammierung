7de39633dbc27e1d27e567a9fbfeb2d5a3df7fc8
Lukas Hatzenbühler <lukashatzenbuehler@Lukass-MBP.fritz.box>
2019-11-17 14:20:08 +0100

    Änderungen

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,8 @@
-
-
+let login1;
 /**
  * Klasse PageOverview: Stellt die Startseite der App zur Verfügung
  */
-class Home {
+class Login {
     /**
      * Konstruktor
      * @param {App} app Zentrale Instanz der App-Klasse
@@ -17,8 +16,8 @@ class Home {
      */
     async show() {
         // Anzuzeigenden Seiteninhalt nachladen
-        let html = await fetch("indexraffa.html");
-        let css = await fetch("indexraffa.css");
+        let html = await fetch("Login/Login.html");
+        let css = await fetch("Login/Login.css");
 
         if (html.ok && css.ok) {
             html = await html.text();
@@ -37,5 +36,24 @@ class Home {
         this._app.setPageCss(css);
         this._app.setPageContent(pageDom.querySelector("main"));
 
+        document.getElementById('login').addEventListener("click",login);
+
+        login1=this;
     }
+
+
+}
+
+function login(){
+let benutzername = document.getElementById('textfeldBenutzername').value;
+let passwort = document.getElementById('textfeldPasswort').value;
+
+ if(login1._app.database.checkLogin(benutzername,passwort)){
+   window.location.href="#/uebersicht/";
+
+ }else {
+
+   alert("Falsche Anmeldedaten");
+ }
+
 }
