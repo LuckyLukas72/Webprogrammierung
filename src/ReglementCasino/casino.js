242d4420bc1a792fbd7a580bd1f68c49ad28d7ab
Lukas Hatzenbühler <lukashatzenbuehler@Lukass-MBP.fritz.box>
2019-11-17 12:21:17 +0100

    Spiel funktioniert

--- src/ReglementCasino/casino.js
+++ src/ReglementCasino/casino.js
@@ -1,10 +1,58 @@
-$( document ).ready(function() {
-    $('#coin').bind("webkitAnimationEnd", myEndFunction);
+/**
+ * Klasse PageOverview: Stellt die Startseite der App zur Verfügung
+ */
+class Casino {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+    }
 
-    $('#coin').bind("animationend", myEndFunction);
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show() {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("ReglementCasino/casino.html");
+        let css = await fetch("ReglementCasino/style.css");
 
-    showBudget();
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+
+
+        this._app.setPageCss(css);
+        this._app.setPageContent(pageDom.querySelector("main"));
+
+        $('#coin').bind("webkitAnimationEnd", myEndFunction);
+
+        $('#coin').bind("animationend", myEndFunction);
+          kontostand = 50;
+          showBudget();
+
+    }
+
+
+}
+
+/**$( document ).ready(function() {
+
+
+
 });
+
+*/
 
 function myEndFunction() {
 
@@ -28,7 +76,7 @@ function myEndFunction() {
 }
 var user_color;
 var result;
-var kontostand = 50.00;
+var kontostand;
 var euro = "€";
 
 
